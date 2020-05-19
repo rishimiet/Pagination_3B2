aceec929e4bbc7c2d9b009afd5e80420dbacb1ab
Rishi <45095219+rishimiet@users.noreply.github.com>
2020-05-19 17:35:33 +0530

    Add files via upload

---
+++ Pagination_3B2/Pagination_3B2/JMSProducer.cs
@@ -0,0 +1,50 @@
+﻿using Apache.NMS;
+using Apache.NMS.Util;
+using Apache.NMS.ActiveMQ;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace Pagination_3B2
+{
+    class JMSProducer
+    {
+        public void sendMessageToDistiller(string txt, string msgg)
+        {
+            //Create the Connection factory
+            try
+            {
+                string url = ConfigurationManager.AppSettings["queueURL"].ToString();
+                string queueName = ConfigurationManager.AppSettings["queueName"].ToString();
+
+                IConnectionFactory factory = new ConnectionFactory(url);
+
+                using (IConnection connection = factory.CreateConnection())
+                using (ISession session = connection.CreateSession())
+                {
+                    IDestination destination = SessionUtil.GetDestination(session, queueName);
+                    Console.WriteLine("Using destination: " + destination);
+
+                    // Create a producer                
+                    using (IMessageProducer producer = session.CreateProducer(destination))
+                    {
+                        // Start the connection so that messages will be processed.
+                        connection.Start();
+                        IMapMessage msg = producer.CreateMapMessage();
+                        string fileName = txt;
+                        string[] arrStr = txt.Split(',');
+
+                        msg.Body.SetString("FILENAME", txt);
+                        msg.Body.SetString("STAGE", msgg);
+                        //msg.Body.SetString("MESSAGE", msgg);
+                        producer.Send(msg);
+                        Console.WriteLine("Message Send for " + txt);
+                    }
+                }
+            }
+            catch (Exception e) { Console.WriteLine(e.Message); }
+        }
+    }
+}
